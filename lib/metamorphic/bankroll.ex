d3c90606a53c4dfb27de73409afa1ef857034fa9
Mark Thayer <markthayer@hey.com>
2023-10-18 14:25:13 -0400

    Update messaging on change plans errors

--- lib/metamorphic/bankroll.ex
+++ lib/metamorphic/bankroll.ex
@@ -12,25 +12,39 @@ defmodule Metamorphic.Bankroll do
   def can_subscribe_to_plan?(customer, plan) do
     memory_count = Metamorphic.Memories.get_count(customer)
 
-
     cond do
       plan[:title] == "Starter" && memory_count > 50 ->
-        {:error, "You must compost some Memories first"}
+        {:error,
+         "Woops, you have #{number_to_string(memory_count)} Memories. You must delete some Memories before switching to this plan."}
 
       plan[:title] == "Lite" && memory_count > 500 ->
-        {:error, "You must compost some Memories first"}
+        {:error,
+         "Woops, you have #{number_to_string(memory_count)} Memories. You must delete some Memories before switching to this plan."}
 
       plan[:title] == "Plus" && memory_count > 5_000 ->
-        {:error, "You must compost some Memories first"}
+        {:error,
+         "Woops, you have #{number_to_string(memory_count)} Memories. You must delete some Memories before switching to this plan."}
 
       plan[:title] == "Pro" && memory_count > 10_000 ->
-        {:error, "You must compost some Memories first"}
+        {:error,
+         "Woops, you have #{number_to_string(memory_count)} Memories. You must delete some Memories before switching to this plan."}
 
       plan[:title] == "Pro AI" && memory_count > 50_000 ->
-        {:error, "You must compost some Memories first"}
+        {:error,
+         "Woops, you have #{number_to_string(memory_count)} Memories. You must delete some Memories before switching to this plan."}
 
       true ->
         :ok
+    end
+  end
+
+  defp number_to_string(number) do
+    case Metamorphic.Cldr.Number.to_string(number) do
+      {:ok, string} ->
+        string
+
+      _rest ->
+        nil
     end
   end
 end
