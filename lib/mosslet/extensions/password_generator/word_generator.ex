9a5a54dbd9587bf0ebe575716af378852236ac79
Mark Thayer <markthayer@hey.com>
2025-07-10 15:11:46 -0400

    Swap crypto for enacl 🔐

--- lib/mosslet/extensions/password_generator/word_generator.ex
+++ lib/mosslet/extensions/password_generator/word_generator.ex
@@ -13,12 +13,13 @@ defmodule Mosslet.Extensions.PasswordGenerator.WordGenerator do
   end
 
   def generate_number do
-    <<a::32, b::32, c::32>> = :crypto.strong_rand_bytes(12)
+    <<a::32, b::32, c::32>> = :enacl.randombytes(12)
     # don't use the default seed to increase randomness
     # :random.seed(a, b, c)
     # Enum.reduce(1..5, 0, fn _, sum -> :random.uniform(6) + sum * 10 end)
     # Use the new :rand.seed/2
     # Currently using the [exro928ss](https://erlang.org/doc/man/rand.html#algorithms) algorithm
+    # we also swap :enacl.randombytes(12) for :crypto.strong_rand_bytes(12)
     :rand.seed(:exro928ss, {a, b, c})
     Enum.reduce(1..5, 0, fn _, sum -> :rand.uniform(6) + sum * 10 end)
   end
