e329707a3df33c6dbf41f3613992d3b087f8c961
Mark Thayer <markthayer@hey.com>
2025-11-14 12:08:54 -0500

    Add unlock_session controller 🔐

---
+++ lib/mosslet_web/controllers/unlock_session_controller.ex
@@ -0,0 +1,49 @@
+defmodule MossletWeb.UnlockSessionController do
+  use MossletWeb, :controller
+
+  alias Mosslet.Accounts
+
+  def new(conn, _params) do
+    if user = get_current_user_from_session(conn) do
+      render(conn, "unlock_session.html",
+        page_title: "Unlock Session",
+        user: user,
+        error_message: get_flash(conn, :error),
+        form: to_form(%{}, as: :unlock),
+        trigger_submit: false
+      )
+    else
+      conn
+      |> put_flash(:error, "Please log in.")
+      |> redirect(to: ~p"/auth/sign_in")
+    end
+  end
+
+  def create(conn, %{"unlock" => %{"password" => password}}) do
+    user = get_current_user_from_session(conn)
+
+    case Accounts.User.valid_key_hash?(user, password) do
+      {:ok, key} ->
+        conn
+        |> put_session(:key, key)
+        |> put_flash(:info, "Session unlocked successfully!")
+        |> redirect(to: ~p"/app")
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid password. Please try again.")
+        |> redirect(to: ~p"/auth/unlock")
+
+      false ->
+        conn
+        |> put_flash(:error, "Invalid password. Please try again.")
+        |> redirect(to: ~p"/auth/unlock")
+    end
+  end
+
+  defp get_current_user_from_session(conn) do
+    if user_token = get_session(conn, :user_token) do
+      Accounts.get_user_by_session_token(user_token)
+    end
+  end
+end
