e0137929e647d73cea05374c8b1b2fa0bd93c04b
Mark Thayer <markthayer@hey.com>
2025-12-05 22:15:31 -0500

    Update migration for 📮

--- priv/repo/migrations/20251201180858_rename_notification_subscription_fields.exs
+++ priv/repo/migrations/20251201180858_rename_notification_subscription_fields.exs
@@ -2,12 +2,34 @@ defmodule Mosslet.Repo.Local.Migrations.RenameNotificationSubscriptionFields do
   use Ecto.Migration
 
   def up do
-    rename table(:users), :is_subscribed_to_marketing_notifications, to: :calm_notifications
-    rename table(:users), :is_subscribed_to_email_notifications, to: :email_notifications
+    if column_exists?(:users, :is_subscribed_to_marketing_notifications) do
+      rename table(:users), :is_subscribed_to_marketing_notifications, to: :calm_notifications
+    end
+
+    if column_exists?(:users, :is_subscribed_to_email_notifications) do
+      rename table(:users), :is_subscribed_to_email_notifications, to: :email_notifications
+    end
   end
 
   def down do
-    rename table(:users), :calm_notifications, to: :is_subscribed_to_marketing_notifications
-    rename table(:users), :email_notifications, to: :is_subscribed_to_email_notifications
+    if column_exists?(:users, :calm_notifications) do
+      rename table(:users), :calm_notifications, to: :is_subscribed_to_marketing_notifications
+    end
+
+    if column_exists?(:users, :email_notifications) do
+      rename table(:users), :email_notifications, to: :is_subscribed_to_email_notifications
+    end
+  end
+
+  defp column_exists?(table, column) do
+    query = """
+    SELECT 1 FROM information_schema.columns
+    WHERE table_name = '#{table}' AND column_name = '#{column}'
+    """
+
+    case repo().query(query) do
+      {:ok, %{num_rows: n}} when n > 0 -> true
+      _ -> false
+    end
   end
 end
