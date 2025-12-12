b3cf1fb4a6e02462342c2753c3a25e355b2f5261
Mark Thayer <markthayer@hey.com>
2025-12-12 08:31:08 -0500

    Test 🧪

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,6 @@
 ExUnit.start()
 
+Application.put_env(:phoenix_test, :endpoint, MossletWeb.Endpoint)
 Ecto.Adapters.SQL.Sandbox.mode(Mosslet.Repo.Local, :manual)
 # Configure Wallaby and set base url for tests
 {:ok, _} = Application.ensure_all_started(:wallaby)
