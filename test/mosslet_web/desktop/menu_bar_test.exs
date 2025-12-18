c6d907da2ec17f25042a03ab649e584767432814
Mark Thayer <markthayer@hey.com>
2025-12-18 13:47:19 -0500

    Update menu bar test 🧪

--- test/mosslet_web/desktop/menu_bar_test.exs
+++ test/mosslet_web/desktop/menu_bar_test.exs
@@ -8,29 +8,29 @@ defmodule MossletWeb.Desktop.MenuBarTest do
       menu = %Desktop.Menu{assigns: %{}}
       {:ok, menu} = MenuBar.mount(menu)
 
-      assert menu.assigns.app_name == "Mosslet"
+      assert menu.assigns.app_name == "MOSSLET"
     end
   end
 
   describe "handle_event/2" do
     test "handles unknown commands gracefully" do
-      menu = %Desktop.Menu{assigns: %{app_name: "Mosslet"}}
+      menu = %Desktop.Menu{assigns: %{app_name: "MOSSLET"}}
       {:noreply, _menu} = MenuBar.handle_event("unknown_command", menu)
     end
   end
 
   describe "handle_info/2" do
     test "handles messages without crashing" do
-      menu = %Desktop.Menu{assigns: %{app_name: "Mosslet"}}
+      menu = %Desktop.Menu{assigns: %{app_name: "MOSSLET"}}
       {:noreply, returned_menu} = MenuBar.handle_info(:some_message, menu)
 
-      assert returned_menu.assigns.app_name == "Mosslet"
+      assert returned_menu.assigns.app_name == "MOSSLET"
     end
   end
 
   describe "render/1" do
     test "returns HEEx template with menu structure" do
-      assigns = %{app_name: "Mosslet"}
+      assigns = %{app_name: "MOSSLET"}
       rendered = MenuBar.render(assigns)
 
       assert %Phoenix.LiveView.Rendered{} = rendered
