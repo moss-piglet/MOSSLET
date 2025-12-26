95eaad9ad70407326ed56a2baff5ce069e1ca538
Mark Thayer <markthayer@hey.com>
2025-12-26 01:35:34 -0500

    Ensure ssl and postgres started on release 🐛

--- lib/mosslet/release.ex
+++ lib/mosslet/release.ex
@@ -9,7 +9,7 @@ defmodule Mosslet.Release do
     load_app()
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true), mode: :temporary)
     end
   end
 
@@ -23,6 +23,8 @@ defmodule Mosslet.Release do
   end
 
   defp load_app do
+    Application.ensure_all_started(:ssl)
+    Application.ensure_all_started(:postgrex)
     Application.load(@app)
   end
 
