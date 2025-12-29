5b480740f20beaa1e9e363b19748515541ad8b7e
Mark Thayer <markthayer@hey.com>
2025-12-29 13:45:37 -0500

    Update runtime for ipv6 👩‍⚕️

--- config/runtime.exs
+++ config/runtime.exs
@@ -118,7 +118,11 @@ if config_env() == :prod do
       encryption_salt: System.get_env("LIVE_VIEW_ENCRYPTION_SALT")
     ],
     http: [
-      ip: {0, 0, 0, 0},
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
