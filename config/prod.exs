304fa9c7390689674df6ce1d0ec3ec2014eab497
Mark Thayer <markthayer@hey.com>
2025-12-29 16:13:47 -0500

    👩‍⚕️

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :mosslet, MossletWeb.Endpoint,
+  adapter: Bandit.PhoenixAdapter,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
