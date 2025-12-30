343d057e5737bf31dc8b7cbb264475b958090aea
Mark Thayer <markthayer@hey.com>
2025-12-30 11:21:19 -0500

    Keep removed cpu_access check

--- lib/mosslet/delayed_serving.ex
+++ lib/mosslet/delayed_serving.ex
@@ -41,17 +41,11 @@ defmodule Mosslet.DelayedServing do
     server = self()
 
     spawn(fn ->
-      if has_cpu_access?() do
-        Logger.info("Elixir has CPU access! Starting serving #{inspect(state.serving_name)}.")
+      Logger.info("Starting serving #{inspect(state.serving_name)}.")
 
-        serving = state.serving_fn.()
-        Logger.info("Serving #{inspect(state.serving_name)} started")
-        send(server, {:serving_loaded, serving})
-      else
-        Logger.warning("Elixir does not have CPU access. Serving will NOT be started.")
-      end
-
-      :ok
+      serving = state.serving_fn.()
+      Logger.info("Serving #{inspect(state.serving_name)} started")
+      send(server, {:serving_loaded, serving})
     end)
 
     # trigger the async callback after GenServer start
@@ -87,31 +81,6 @@ defmodule Mosslet.DelayedServing do
     rescue
       _exception ->
         Logger.error("Error trying to determine GPU access!")
-        false
-    end
-  end
-
-  @doc """
-  Return if Elixir has access to the GPU or not.
-  """
-  @spec has_cpu_access? :: boolean()
-  def has_cpu_access?() do
-    try do
-      case Nx.tensor(0) do
-        # :host == CPU
-        %Nx.Tensor{data: %EXLA.Backend{buffer: %EXLA.DeviceBuffer{client_name: :host}}} ->
-          true
-
-        # :cuda == GPU
-        %Nx.Tensor{data: %EXLA.Backend{buffer: %EXLA.DeviceBuffer{client_name: :cuda}}} ->
-          false
-
-        _other ->
-          false
-      end
-    rescue
-      _exception ->
-        Logger.error("Error trying to determine CPU access!")
         false
     end
   end
