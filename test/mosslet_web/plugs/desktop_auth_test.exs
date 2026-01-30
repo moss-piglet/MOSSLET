7870cc3f28fbb07829388f0d10428812c693fa4d
Mark Thayer <markthayer@hey.com>
2026-01-30 16:29:04 -0500

    Update tests and log notarization failure

--- test/mosslet_web/plugs/desktop_auth_test.exs
+++ test/mosslet_web/plugs/desktop_auth_test.exs
@@ -1,29 +1,31 @@
-defmodule MossletWeb.Plugs.DesktopAuthTest do
-  use MossletWeb.ConnCase, async: true
+if System.get_env("MOSSLET_NATIVE") == "true" do
+  defmodule MossletWeb.Plugs.DesktopAuthTest do
+    use MossletWeb.ConnCase, async: true
 
-  alias MossletWeb.Plugs.DesktopAuth
+    alias MossletWeb.Plugs.DesktopAuth
 
-  describe "init/1" do
-    test "returns opts unchanged" do
-      opts = [some: :option]
-      assert DesktopAuth.init(opts) == opts
+    describe "init/1" do
+      test "returns opts unchanged" do
+        opts = [some: :option]
+        assert DesktopAuth.init(opts) == opts
+      end
+
+      test "returns empty opts" do
+        assert DesktopAuth.init([]) == []
+      end
     end
 
-    test "returns empty opts" do
-      assert DesktopAuth.init([]) == []
-    end
-  end
+    describe "module behavior" do
+      test "implements Plug behaviour" do
+        behaviours = MossletWeb.Plugs.DesktopAuth.__info__(:attributes)[:behaviour] || []
+        assert Plug in behaviours
+      end
 
-  describe "module behavior" do
-    test "implements Plug behaviour" do
-      behaviours = MossletWeb.Plugs.DesktopAuth.__info__(:attributes)[:behaviour] || []
-      assert Plug in behaviours
-    end
-
-    test "exports init/1 and call/2" do
-      exports = MossletWeb.Plugs.DesktopAuth.__info__(:functions)
-      assert {:init, 1} in exports
-      assert {:call, 2} in exports
+      test "exports init/1 and call/2" do
+        exports = MossletWeb.Plugs.DesktopAuth.__info__(:functions)
+        assert {:init, 1} in exports
+        assert {:call, 2} in exports
+      end
     end
   end
 end
